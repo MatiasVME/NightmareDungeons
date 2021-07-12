b30a07e17326118c0983c456bdfd45ed7e096433
Kuba Dajek <asessinerx10@gmail.com>
2021-07-13 01:40:22 +0200

    Added a startup message

--- src/main/java/mc/nightmarephoenix/nightmaredungeons/NightmareDungeons.java
+++ src/main/java/mc/nightmarephoenix/nightmaredungeons/NightmareDungeons.java
@@ -1,6 +1,8 @@
 package mc.nightmarephoenix.nightmaredungeons;
 
 import mc.nightmarephoenix.nightmaredungeons.commands.NDCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NightmareDungeons extends JavaPlugin {
@@ -8,7 +10,7 @@ public final class NightmareDungeons extends JavaPlugin {
     @Override
     public void onEnable() {
 
-
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN+"NightmareDungeons plugin has been activated!");
         /**
          * Loading commands
          */
