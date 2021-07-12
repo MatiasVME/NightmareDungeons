4489e96c9da80ab7b671652b88f48be3444e3ad2
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2021-07-12 17:56:19 -0400

    Base project

---
+++ src/main/java/mc/nightmarephoenix/nightmaredungeons/NightmareDungeons.java
@@ -0,0 +1,17 @@
+package mc.nightmarephoenix.nightmaredungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NightmareDungeons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
